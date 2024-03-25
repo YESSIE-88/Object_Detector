89a52e8cde32c3ad350243b898ca77f318143453
Jessie_48 <116089615+YESSIE-88@users.noreply.github.com>
2024-03-24 20:20:17 -0400

    Add files via upload

---
+++ Object_Detect.py
@@ -0,0 +1,60 @@
+import cv2
+from picamera2 import Picamera2
+
+#thres = 0.45 # Threshold to detect object
+
+classNames = []
+classFile = "/home/jessie/Desktop/Object_Detection_Files/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/jessie/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/jessie/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+    #print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    return img,objectInfo
+
+
+if __name__ == "__main__":
+
+    piCam = Picamera2()
+    piCam.preview_configuration.main.size=(1920, 1080)
+    piCam.preview_configuration.main.format = "RGB888"
+    piCam.preview_configuration.align()
+    piCam.configure("preview")
+    piCam.start()
+
+
+    while True:
+        frame = piCam.capture_array()
+        result, objectInfo = getObjects(frame,0.5 ,0.2, objects = ['cup', 'banana'])
+        #result, objectInfo = getObjects(frame,0.5 ,0.2)
+        #print(objectInfo)
+        cv2.imshow("Output",frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+cv2.destroyAllWindows()
+
